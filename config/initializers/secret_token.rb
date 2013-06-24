5d6247b909150b6f231bcbc0d5126b3643d6f72c
dbc-apprentice <apprentice@devbootcamp.com>
2013-06-24 11:27:07 -0500

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+HackerNews::Application.config.secret_token = '43494bf117e1c1339bec2acc91e1d5c85b0580e4f418cb932d06b7b414c6b689fe554e91b876a25f0f50002b81b3a27aea7d9d6fa5923cfcce09dbe176a23a2e'
