ae755294d2d1d84447904810c5644cb675f47988
dbc-apprentice <apprentice@devbootcamp.com>
2013-06-24 16:12:36 -0500

    changed from flash notice to flash error

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -16,7 +16,7 @@ class PostsController < ApplicationController
     if @post.create
       redirect_to post_path(@post.id)
     else
-      flash[:notice] = "Boo sad sad."
+      flash[:error] = "Boo sad sad."
       redirect_to new_post_path
     end
   end
